2af4a65f19f59509df6165706e693e02272f4258
Júlia Marques <jumarques631@gmail.com>
2026-07-04 13:55:03 -0300

    chore: reorganiza estrutura de pastas e remove boilerplate padrão do Vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import HomePrincipal from './pages/DashboardPrincipal/index'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HomePrincipal/>
   </StrictMode>,
 )
---
+++ frontend/src/pages/DashboardPrincipal/index.jsx
@@ -0,0 +1,10 @@
+function HomePrincipal() {
+
+  return (
+    <div style={{backgroundColor: '#ffff'}}>
+        <h1>Olá React!</h1>
+    </div>
+  )
+}
+
+export default HomePrincipal
